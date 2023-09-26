0a8832b8c3613f9194fa13e506add178a3ab5a67
marekcapliar <113228472+marekcapliar@users.noreply.github.com>
2023-09-26 19:23:06 +0200

    Update mena.py

--- 16/mena.py
+++ 16/mena.py
@@ -5,7 +5,7 @@ longest_first_name = sorted(zoz[:len(zoz)//2 - 1:], reverse=True)[0]
 longest_last_name = sorted(zoz[len(zoz)//2::], reverse=True)[0]
 print("Dlzka najdlhsieho krstneho mena:", len(longest_first_name))
 print("Dlzka najdlhsieho priezviska je", len(longest_last_name))
-print(zoz)
+
 fw = open("kucera/16/vystup.txt", 'w', encoding="UTF-8")
 spaces = len(longest_first_name)
 for i in range(len(zoz)//2):
